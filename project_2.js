d2341ddcce51f68e0e33445e289d676e1f241dc9
coskunrumeysa <70705198+coskunrumeysa@users.noreply.github.com>
2022-01-10 15:36:21 +0300

    Add files via upload

---
+++ project_2.js
@@ -0,0 +1,6 @@
+//Beni Yaratan Elbet Yolumu Gösterir.Suara /78.
+// This program written for do divide operation at Js.
+number_1 = 2
+number_2 = 2
+division =  (number_1/number_2)
+console.log(division)
